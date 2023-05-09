aed7c40fca9c0b16abd882e5dea3f067f67fcef5
1ethansempa <esempa@sailglobalcorp.com>
2023-05-09 13:58:43 +0300

    build: expense app

---
+++ expense-app/src/custom.interceptor.ts
@@ -0,0 +1,24 @@
+/* This isnt needed */
+
+import { NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
+import { map } from 'rxjs';
+
+export class CustomInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, handler: CallHandler) {
+    console.log('This is intercepting the request');
+    console.log(context);
+
+    return handler.handle().pipe(
+      map((data) => {
+        const response = {
+          ...data,
+          createdAt: data.created_at,
+        };
+        delete response.updated_at;
+        delete response.created_at;
+
+        return response;
+      }),
+    );
+  }
+}
